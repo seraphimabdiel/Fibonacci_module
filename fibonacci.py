9a802344334d59ac8bbb5a171190ea41991800bd
Angus Yu <seraphim.abdiel@gmail.com>
2014-11-22 11:41:15 +0800

    fibonacci module
    python homework

---
+++ fibonacci.py
@@ -0,0 +1,5 @@
+def fibonacci(num):
+   a, b, c, n = 0, 1, 0, 0
+
+   for n in range(0, num):
+     print c; a = b; b = c; c = a + b
---
+++ fibonacciwithnodef.py
@@ -0,0 +1,4 @@
+from fibonacci import*
+num = int(raw_input('How many number do you want to be shown?'))
+
+fibonacci(num)
