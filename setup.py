20dc073a9b0abb89c72455efabe32dcd7def6713
Angus Yu <seraphim.abdiel@gmail.com>
2014-11-22 11:46:42 +0800

    setup
    setup fibonacci module

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+setup(name='Fibonacci_module',
+version='1.0',
+description='python homework',
+url='https://github.com/seraphimabdiel/Fibonacci_module',
+author='seraphimabdiel',
+author_email='seraphim.abdiel@gmail.com',
+packages = find_packages(),
+)
